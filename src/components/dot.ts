797c00142b8bd475ac2c0c8d0c04981dd69ce565
Lauren Sauvillers <lauren@sauvillers.be>
2025-11-09 12:59:10 +0100

    Improved touch controls

--- src/components/dot.ts
+++ src/components/dot.ts
@@ -1,14 +1,20 @@
 export class Dot {
   private context: CanvasRenderingContext2D;
+  private touch: Touch
 
-  constructor(context: CanvasRenderingContext2D) {
+  constructor(context: CanvasRenderingContext2D, touch: Touch) {
     this.context = context;
+    this.touch = touch;
   }
 
-  public draw(location: { x: number, y: number }) {
+  public updateTouch(touch: Touch) {
+    this.touch = touch;
+  }
+
+  public draw() {
     this.context.fillStyle = "red";
     this.context.beginPath();
-    this.context.arc(location.x, location.y, 40, 0, 2 * Math.PI);
+    this.context.arc(this.touch.clientX, this.touch.clientY, 40, 0, 2 * Math.PI);
     this.context.fill();
   }
 }
